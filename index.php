113b76c2f3fb574806c392880654e3503a651d05
Isaac Anderson <isaacdanielanderson@gmail.com>
2018-08-11 21:34:46 -0500

    Lec. 9 end
    WordPress loops

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
-$names = array('brad', 'john', 'jane', 'meowsalot');
-// Basic loop while loop
-$count = 1;
-while ($count < 100) {
-  echo "<li>$count</li>";
-  $count++; // Adds 1
-}
-?>
-
-<!--  While loop and an array and counter variable -->
-<?php
-$names = array('brad', 'john', 'jane', 'meowsalot');
-$count = 0; // Access first item in array with "0"
-while ($count < count($names)) {
-  echo "<li>Hi my name is $names[$count] </li>";
-  $count++;
-}
-?>
+  while (have_posts()) {
+    the_post(); // This function keeps track of which post we are currently working with ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+    <?php }
+ ?>
