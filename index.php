30c0a12a1c8a026dc28f8652b2091fbb757de265
Isaac Anderson <isaacdanielanderson@gmail.com>
2018-08-11 21:24:17 -0500

    Lec. 8 end
    Arrays and loops

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-This is our amazing custom theme.
+<?php
+$names = array('brad', 'john', 'jane', 'meowsalot');
+// Basic loop while loop
+$count = 1;
+while ($count < 100) {
+  echo "<li>$count</li>";
+  $count++; // Adds 1
+}
+?>
+
+<!--  While loop and an array and counter variable -->
+<?php
+$names = array('brad', 'john', 'jane', 'meowsalot');
+$count = 0; // Access first item in array with "0"
+while ($count < count($names)) {
+  echo "<li>Hi my name is $names[$count] </li>";
+  $count++;
+}
+?>
