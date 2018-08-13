21714ed590c026f53e0c7e3de7ef27dfbb36fc33
Isaac Anderson <isaacdanielanderson@gmail.com>
2018-08-13 15:48:58 -0500

    Lec. 23 End
    Few quick edits

--- front-page.php
+++ front-page.php
@@ -57,13 +57,13 @@
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray"> Read more</a></p>
               </div>
             </div>
         <?php } wp_reset_postdata(); ?>
 
 
-        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
     </div>
   </div>
