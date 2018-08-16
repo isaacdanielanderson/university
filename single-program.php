9f6b1596ed5f5582f856afaa1b238f8abbb59e0f
Isaac Anderson <isaacdanielanderson@gmail.com>
2018-08-16 18:58:31 -0500

    Lec. 37 End
    Quick program edits

--- single-program.php
+++ single-program.php
@@ -1,9 +1,8 @@
 <?php
-
-  get_header();
-
-  while(have_posts()) {
-    the_post(); ?>
+get_header();
+// While loop
+while(have_posts()) {
+  the_post(); ?>
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
       <div class="page-banner__content container container--narrow">
@@ -43,35 +42,32 @@
             )
           )
         ));
-
+        if ($homepageEvents->have_posts()) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' events</h2>';
+        }
         while($homepageEvents->have_posts()) {
           $homepageEvents->the_post(); ?>
           <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month"><?php
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate->format('M')
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
               ?></span>
               <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
               <p><?php if (has_excerpt()) {
-                  echo get_the_excerpt();
-                } else {
-                  echo wp_trim_words(get_the_content(), 18);
-                  } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                echo get_the_excerpt();
+              } else {
+                echo wp_trim_words(get_the_content(), 18);
+              } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
             </div>
           </div>
-        <?php }
-      ?>
-
+        <?php } ?>
     </div>
 
+<?php } ?>
 
-
-  <?php }
-
-  get_footer();
-
-?>z
+<?php get_footer(); ?>
