822eae44bc40ee85c45aab78f68b21bee4cc7ee2
Isaac Anderson <isaacdanielanderson@gmail.com>
2018-09-11 22:31:26 -0500

    Lec. 39 End
    Regenerate thumnails, add_image_size();

--- functions.php
+++ functions.php
@@ -13,8 +13,10 @@ function university_features() {
   register_nav_menu('header-menu-location', 'Header Menu Location');
   register_nav_menu('footerLocation1', 'Footer Menu Location 1');
   register_nav_menu('footerLocation2', 'Footer Menu Location 2');
-
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
 }
 add_action('after_setup_theme', 'university_features');
 
--- single-professor.php
+++ single-professor.php
@@ -18,7 +18,17 @@
       <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
-      <div class="generic-content"><?php the_content(); ?></div>
+
+      <div class="generic-content">
+        <div class="row group">
+          <div class="one-third">
+            <?php the_post_thumbnail(); ?>
+          </div>
+          <div class="two-thirds">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
 
       <?php
         $relatedPrograms = get_field('related_programs');
